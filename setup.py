c11e882860a163a68c0c165341e9d5c5bea76ec7
mebratu <mebratucheka7@gmail.com>
2026-08-29 17:47:04 +0300

    ingestion stage completed

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Chest-Disease-Detection"
+REPO_NAME = "AemroVision-Brain-Tumor-Classifier"
 AUTHOR_USER_NAME = "mebre7"
 SRC_REPO = "cnnClassifier"
 AUTHOR_EMAIL = "mebratucheka7@gmail.com"
@@ -15,12 +15,12 @@ setuptools.setup(
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description="A small package for CNN classifier",
+    description="A small package for cnn model for brain tumor classification",
     long_description=long_description,
     long_description_content="text/markdown",
-    url=f"https://github/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
-        "Bug Tracker": f"https://github/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src")
